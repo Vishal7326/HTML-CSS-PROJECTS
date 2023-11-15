77c5f9e57bb04ef60d3dd7c52e45f9890ffa75e1
Vishal <147958395+Vishal7326@users.noreply.github.com>
2023-11-15 11:39:16 +0530

    Add files via upload

---
+++ Razorpay1/tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      fontFamily:{
+        mullish:["Mullish","sans-serif"],
+      },
+      colors:{
+        deepblue:"#02042a",
+        lighblue:"#2b84ea"
+      }
+    },
+  },
+  plugins: [],
+}
